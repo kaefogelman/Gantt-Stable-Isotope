9955fa65e13e8d12c9ef4d8de377580f50d13e06
Kaelyn Fogelman <69921597+kaefogelman@users.noreply.github.com>
2021-08-25 14:36:28 -0500

    Gantt PIP completed

--- Gantt Lake Stable Isotope/GanttSI.R
+++ Gantt Lake Stable Isotope/GanttSI.R
@@ -1,6 +1,6 @@
-mix.filename <- read.csv("Gantt_mussels.csv")
-source.filename <- read.csv("Gantt_sources_notcombined.csv")
-discr.filename <- read.csv("Gantt_TDF10.csv")
+mix.filename <- read.csv("mussels_withoutoutliers.csv")
+source.filename <- read.csv("Gantt_sources_combined.csv")
+discr.filename <- read.csv("post.tdf.csv")
 
 install.packages("MixSIAR")
 library(MixSIAR)
@@ -31,87 +31,9 @@ output_options <- list(summary_save = TRUE,
                        plot_xy_save_png = FALSE,
                        return_obj = TRUE)	
 
+#############################################################################
 ##NULL MODEL
-mix <- load_mix_data(filename="Gantt_mussels.csv", 
-                     iso_names=c("d13C","d15N"),
-                     factors=NULL, 
-                     fac_random=NULL,
-                     fac_nested=NULL,
-                     cont_effects=NULL)
-
-source <- load_source_data(filename="Gantt_sources_notcombined.csv",
-                           source_factors=NULL,
-                           conc_dep=FALSE,
-                           data_type="means",
-                           mix)
-
-source.filename<-read.csv("Gantt_sources_notcombined.csv")
-
-discr <- load_discr_data(filename="Gantt_TDF10.csv", mix)
-
-plot_data(filename="isospace_plot", 	
-          plot_save_pdf=FALSE,	
-          plot_save_png=FALSE,	
-          mix,source,discr)
-
-write_JAGS_model(model_filename, resid_err, process_err, mix, source)
-
-jags.1 <- run_model(run = "test", mix, source, discr, model_filename, 
-                    alpha.prior=1, resid_err, process_err)
-
-jags.1 <- run_model(run = "long", mix, source, discr, model_filename, 
-                    alpha.prior=1, resid_err, process_err)
-
-diag.null <- output_JAGS(jags.1, mix, source, output_options)
-
-##Species
-mix <- load_mix_data(filename="Gantt_mussels.csv", 
-                     iso_names=c("d13C","d15N"),
-                     factors="Species", 
-                     fac_random=FALSE,
-                     fac_nested=FALSE,
-                     cont_effects=NULL)
-
-source <- load_source_data(filename="Gantt_sources_notcombined.csv",
-                           source_factors=NULL,
-                           conc_dep=FALSE,
-                           data_type="means",
-                           mix)
-
-source.filename<-read.csv("Gantt_sources_notcombined.csv")
-
-discr <- load_discr_data(filename="Gantt_TDF10.csv", mix)
-
-plot_data(filename="isospace_plot", 	
-          plot_save_pdf=FALSE,	
-          plot_save_png=FALSE,	
-          mix,source,discr)
-
-write_JAGS_model(model_filename, resid_err, process_err, mix, source)
-
-jags.2 <- run_model(run = "test", mix, source, discr, model_filename, 
-                    alpha.prior=1, resid_err, process_err)
-
-jags.2 <- run_model(run = "normal", mix, source, discr, model_filename, 
-                    alpha.prior=1, resid_err, process_err)
-
-diag.species <- output_JAGS(jags.2, mix, source, output_options)
-
-##Species - Lit Prior 
-
-lit.prior = c(0.2,0.2,0.6)
-
-jags.3 <- run_model(run = "test", mix, source, discr, model_filename, 
-                    alpha.prior=lit.prior, resid_err, process_err)
-
-jags.3 <- run_model(run = "very short", mix, source, discr, model_filename, 
-                    alpha.prior=lit.prior, resid_err, process_err)
-
-lit.prior <- output_JAGS(jags.3, mix, source, output_options)
-
-#####################################
-###SIMPLIFIED SOURCES - NULL MODEL###
-mix <- load_mix_data(filename="Gantt_mussels.csv", 
+mix <- load_mix_data(filename="mussels_withoutoutliers.csv", 
                      iso_names=c("d13C","d15N"),
                      factors=NULL, 
                      fac_random=NULL,
@@ -124,12 +46,9 @@ source <- load_source_data(filename="Gantt_sources_combined.csv",
                            data_type="means",
                            mix)
 
-test1 <- read.csv(path, header=TRUE)
+source.filename<-read.csv("Gantt_sources_combined.csv")
 
-source.filename <- read.csv("Gantt_sources_notcombined.csv")
-source.filename <- read.csv("Gantt_sources_combined2.csv", header = TRUE)
-
-discr <- load_discr_data(filename="Gantt_TDF10.csv", mix)
+discr <- load_discr_data(filename="post.tdf.csv", mix)
 
 plot_data(filename="isospace_plot", 	
           plot_save_pdf=FALSE,	
@@ -145,3 +64,50 @@ jags.1 <- run_model(run = "long", mix, source, discr, model_filename,
                     alpha.prior=1, resid_err, process_err)
 
 diag.null <- output_JAGS(jags.1, mix, source, output_options)
+
+#############################################################################
+##Species
+mix <- load_mix_data(filename="Gantt_mussels.csv", 
+                     iso_names=c("d13C","d15N"),
+                     factors="?..Species", 
+                     fac_random=FALSE,
+                     fac_nested=FALSE,
+                     cont_effects=NULL)
+
+source <- load_source_data(filename="Gantt_sources_combined.csv",
+                           source_factors=NULL,
+                           conc_dep=FALSE,
+                           data_type="means",
+                           mix)
+
+source.filename<-read.csv("Gantt_sources_combined.csv")
+
+discr <- load_discr_data(filename="Gantt_TDF3.csv", mix)
+
+plot_data(filename="isospace_plot", 	
+          plot_save_pdf=FALSE,	
+          plot_save_png=FALSE,	
+          mix,source,discr)
+
+write_JAGS_model(model_filename, resid_err, process_err, mix, source)
+
+jags.2 <- run_model(run = "test", mix, source, discr, model_filename, 
+                    alpha.prior=1, resid_err, process_err)
+
+jags.2 <- run_model(run = "very long", mix, source, discr, model_filename, 
+                    alpha.prior=1, resid_err, process_err)
+
+diag.species <- output_JAGS(jags.2, mix, source, output_options)
+
+############################################################################
+##Species - Lit Prior 
+
+lit.prior = c(0.2,0.2,0.6)
+
+jags.3 <- run_model(run = "test", mix, source, discr, model_filename, 
+                    alpha.prior=lit.prior, resid_err, process_err)
+
+jags.3 <- run_model(run = "very short", mix, source, discr, model_filename, 
+                    alpha.prior=lit.prior, resid_err, process_err)
+
+lit.prior <- output_JAGS(jags.3, mix, source, output_options)
